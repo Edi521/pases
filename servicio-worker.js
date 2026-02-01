7ea80fd5dd8a2a948b00f08715b98b1efc7f3c4f
Hey_Edii <121597923+Edi521@users.noreply.github.com>
2026-02-01 01:10:01 -0600

    Add files via upload

---
+++ servicio-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Cache mínimo opcional (puedes dejarlo así por ahora)
+self.addEventListener("fetch", () => {});
